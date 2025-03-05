9fd3e253358f3136384e2a7959fde73a0280d413
wallinrex <wallinrex@gmail.com>
2025-03-04 20:43:54 -0600

    Added javadocs to Block.java

--- src/main/java/edu/grinnell/csc207/blockchain/Block.java
+++ src/main/java/edu/grinnell/csc207/blockchain/Block.java
@@ -39,6 +39,12 @@ public class Block {
         this.currHash = new Hash(calculateHash());
     }
 
+    /**
+     * Calculates the hash for a block based on its number, the amount of the corresponding
+     * transaction, the previous hash (if applicable), and the nonce
+     * @return the calculated hash in a byte array
+     * @throws NoSuchAlgorithmException if MessageDigest.getInstance is passed an invalid algorithm
+     */
     public byte[] calculateHash() throws NoSuchAlgorithmException {
         MessageDigest md = MessageDigest.getInstance("sha-256");
         md.update(ByteBuffer.allocate(4).putInt(this.num).array());
@@ -51,26 +57,50 @@ public class Block {
         return hash;
     }
 
+    /**
+     * Getter for the num field of a block
+     * @return the block's number
+     */
     public int getNum() {
         return this.num;
     }
 
+    /**
+     * Getter for the amount field of a block
+     * @return the amount of the transaction the block records
+     */
     public int getAmount() {
         return this.amount;
     }
 
+    /**
+     * Getter for the nonce field of a block
+     * @return the block's nonce
+     */
     public long getNonce() {
         return this.nonce;
     }
 
+    /**
+     * Getter for the prevHash field of a block
+     * @return the hash of the previous block
+     */
     public Hash getPrevHash() {
         return this.prevHash;
     }
 
+    /**
+     * Getter for the currHash field of a block
+     * @return the block's hash
+     */
     public Hash getHash() {
         return this.currHash;
     }
 
+    /**
+     * Returns a string representation of the block
+     * @return a string containing all the fields of the block
+     */
     public String toString() {
         return String.format("Block %d (Amount: %d, Nonce: %d, prevHash: %s, hash: %s)",
                              this.num, this.amount, this.nonce, this.prevHash.toString(),
